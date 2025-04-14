a878fd8368a916c7ffffc3f197879834c8d9e661
kulikp1 <pashakylik2@gmail.com>
2025-04-14 18:54:04 +0300

    add order page

---
+++ src/components/OrderPage/OrderPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./OrderPage.module.css";
+
+const Order = () => {
+  return (
+    <div className={styles.page}>
+      <h1>Order</h1>
+    </div>
+  );
+};
+
+export default Order;
