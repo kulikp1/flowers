3bbd10def7f952160af4d27fcb07d2e212a26526
kulikp1 <pashakylik2@gmail.com>
2025-04-14 18:51:23 +0300

    add CatalogPage

---
+++ src/components/CatalogPage/CatalogPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./CatalogPage.module.css";
+
+const Catalog = () => {
+  return (
+    <div className={styles.page}>
+      <h1>Catalog</h1>
+    </div>
+  );
+};
+
+export default Catalog;
