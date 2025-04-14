760767561994c255e62f1307d74333b65a3b46b7
kulikp1 <pashakylik2@gmail.com>
2025-04-14 18:54:18 +0300

    update routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../HomePage/HomePage";
+import Catalog from "../CatalogPage/CatalogPage";
+import Order from "../OrderPage/OrderPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<Catalog />} />
+        <Route path="/orders" element={<Order />} />
       </Routes>
     </Router>
   );
