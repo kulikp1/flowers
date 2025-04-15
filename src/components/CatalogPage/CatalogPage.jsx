171838037d4ceea906892f2e9e544979a00d71b1
kulikp1 <pashakylik2@gmail.com>
2025-04-15 23:27:32 +0300

    update style

--- src/components/CatalogPage/CatalogPage.jsx
+++ src/components/CatalogPage/CatalogPage.jsx
@@ -9,31 +9,33 @@ const Catalog = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
-      <h2>Каталог квітів</h2>
-      <div className={styles.grid}>
-        {flowers.map((flower) => (
-          <div key={flower.id} className={styles.card}>
-            <img
-              src={flower.image}
-              alt={flower.name}
-              className={styles.image}
-            />
+      <div className={styles.container}>
+        <h2>Каталог квітів</h2>
+        <div className={styles.grid}>
+          {flowers.map((flower) => (
+            <div key={flower.id} className={styles.card}>
+              <img
+                src={flower.image}
+                alt={flower.name}
+                className={styles.image}
+              />
 
-            <h3>{flower.name}</h3>
-            <p>В наявності: {flower.quantity} шт.</p>
-            <p>
-              Ціна: <strong>{flower.price} грн</strong>
-            </p>
-            {flower.oldPrice && flower.oldPrice !== flower.price && (
-              <p className={styles.oldPrice}>{flower.oldPrice} грн</p>
-            )}
-            <button onClick={() => handleAddToCart(flower)}>
-              Додати до кошика
-            </button>
-          </div>
-        ))}
+              <h3>{flower.name}</h3>
+              <p>В наявності: {flower.quantity} шт.</p>
+              <p>
+                Ціна: <strong>{flower.price} грн</strong>
+              </p>
+              {flower.oldPrice && flower.oldPrice !== flower.price && (
+                <p className={styles.oldPrice}>{flower.oldPrice} грн</p>
+              )}
+              <button onClick={() => handleAddToCart(flower)}>
+                Додати до кошика
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
