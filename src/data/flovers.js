65cfd1557f8fd3813c80bd48cc32d25fc9504db2
kulikp1 <pashakylik2@gmail.com>
2025-04-15 23:09:32 +0300

    Create flovers.js

---
+++ src/data/flovers.js
@@ -0,0 +1,25 @@
+const flowers = [
+  {
+    id: 1,
+    name: "Троянда червона",
+    price: 50,
+    oldPrice: 60,
+    quantity: 120,
+  },
+  {
+    id: 2,
+    name: "Тюльпан жовтий",
+    price: 30,
+    oldPrice: 35,
+    quantity: 80,
+  },
+  {
+    id: 3,
+    name: "Хризантема біла",
+    price: 45,
+    oldPrice: 45,
+    quantity: 60,
+  },
+];
+
+export default flowers;
