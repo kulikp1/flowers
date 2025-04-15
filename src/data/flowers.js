5ee75191e8b2c3e802407e5fd98d65c1bd7bf3c7
kulikp1 <pashakylik2@gmail.com>
2025-04-15 23:18:53 +0300

    Update flowers.js

--- src/data/flowers.js
+++ src/data/flowers.js
@@ -2,24 +2,67 @@ const flowers = [
   {
     id: 1,
     name: "Троянда червона",
-    price: 50,
-    oldPrice: 60,
-    quantity: 120,
+    quantity: 50,
+    price: 45,
+    oldPrice: 55,
+    image: "https://images.unsplash.com/photo-1606925797304-5d6b3fc2c5b5"
   },
   {
     id: 2,
-    name: "Тюльпан жовтий",
+    name: "Тюльпан білий",
+    quantity: 70,
     price: 30,
     oldPrice: 35,
-    quantity: 80,
+    image: "https://images.unsplash.com/photo-1587019155557-8e907c843b07"
   },
   {
     id: 3,
-    name: "Хризантема біла",
-    price: 45,
-    oldPrice: 45,
-    quantity: 60,
+    name: "Півонія рожева",
+    quantity: 30,
+    price: 60,
+    oldPrice: 65,
+    image: "https://images.unsplash.com/photo-1599566150163-29194dcaad36"
   },
+  {
+    id: 4,
+    name: "Гербера жовта",
+    quantity: 90,
+    price: 25,
+    oldPrice: 30,
+    image: "https://images.unsplash.com/photo-1562007901-7e3cf50c7b43"
+  },
+  {
+    id: 5,
+    name: "Лілія біла",
+    quantity: 40,
+    price: 50,
+    oldPrice: 55,
+    image: "https://images.unsplash.com/photo-1592179909600-6e9ba3c91b4a"
+  },
+  {
+    id: 6,
+    name: "Хризантема фіолетова",
+    quantity: 60,
+    price: 35,
+    oldPrice: 38,
+    image: "https://images.unsplash.com/photo-1615471505795-b1854ac2e03a"
+  },
+  {
+    id: 7,
+    name: "Орхідея рожева",
+    quantity: 25,
+    price: 70,
+    oldPrice: 80,
+    image: "https://images.unsplash.com/photo-1546435770-a3e426bf472b"
+  },
+  {
+    id: 8,
+    name: "Соняшник",
+    quantity: 100,
+    price: 20,
+    oldPrice: 25,
+    image: "https://images.unsplash.com/photo-1535909339361-8b2b6afafc7b"
+  }
 ];
 
 export default flowers;
