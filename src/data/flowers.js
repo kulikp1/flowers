12d80f6abec72b128fa42ef1c60af3fbbbd6fcf7
kulikp1 <pashakylik2@gmail.com>
2025-04-19 17:23:17 +0300

    Update flowers.js

--- src/data/flowers.js
+++ src/data/flowers.js
@@ -63,22 +63,7 @@ const flowers = [
     oldPrice: 25,
     image: "https://images.prom.ua/5017916024_sonyashnik-odinarnij-58.jpg"
   }
-  {
-    id: 8,
-    name: "Соняшник",
-    quantity: 100,
-    price: 20,
-    oldPrice: 25,
-    image: "https://images.prom.ua/5017916024_sonyashnik-odinarnij-58.jpg"
-  },
-  {
-    id: 9,
-    name: "Соняшник",
-    quantity: 100,
-    price: 20,
-    oldPrice: 25,
-    image: "https://images.prom.ua/5017916024_sonyashnik-odinarnij-58.jpg"
-  }
+ 
 ];
 
 export default flowers;
