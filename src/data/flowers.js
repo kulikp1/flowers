4aad25a7e5f449a93347b9e0fe79280962835a22
kulikp1 <pashakylik2@gmail.com>
2025-04-20 17:06:23 +0300

    Update flowers.js

--- src/data/flowers.js
+++ src/data/flowers.js
@@ -5,14 +5,7 @@ const flowers = [
   
   
   
-  {
-    id: 8,
-    name: "Соняшник",
-    quantity: 100,
-    price: 20,
-    oldPrice: 25,
-    image: "https://images.prom.ua/5017916024_sonyashnik-odinarnij-58.jpg"
-  }
+ 
  
 ];
 
